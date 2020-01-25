fc06758e6d2f699c8e4d261c19b704c59ba9b858
pavan <pavan1645@gmail.com>
2020-01-25 12:37:40 +0530

    Structural and data changes

--- public/App.js
+++ public/App.js
@@ -1,12 +1,33 @@
+const NAME = "Pavankumar Mahadik";
+
 class App extends React.Component {
 	render() {
 		return React.createElement(
 			"div",
-			null,
+			{ className: "container border" },
 			React.createElement(
 				"h1",
+				{ className: "border" },
+				NAME
+			),
+			React.createElement("img", { className: "border", src: "https://image-uploading-service.s3.us-west-1.amazonaws.com/1579935258989_linkedin-profile-compressed.jpg", width: "300px" }),
+			React.createElement(
+				"p",
 				null,
-				"hello"
+				"A Fullstack Web and Android Developer from San Diego. International Graduate Student at San Diego State University pursuing Masters in Computer Science with an expected graduation date of May 2021. Actively seeking internships in Software Development for Summer 2020."
+			),
+			React.createElement(
+				"a",
+				{ href: "https://github.com/pavan-sdsu", target: "_blank" },
+				React.createElement(
+					"button",
+					{ className: "border" },
+					React.createElement(
+						"h3",
+						null,
+						"VIEW MY GITHUB REPO"
+					)
+				)
 			)
 		);
 	}
