d9298b5ad88bdef7e5d947d4ef5d1201b82ad7cd
pavan <pavan1645@gmail.com>
2020-02-03 17:25:24 -0800

    remove class

--- public/App.js
+++ public/App.js
@@ -1,36 +1,32 @@
 const NAME = "Pavankumar Mahadik";
 
-class App extends React.Component {
-	render() {
-		return React.createElement(
-			"div",
-			{ className: "container border" },
+const element = React.createElement(
+	"div",
+	{ className: "container border" },
+	React.createElement(
+		"h1",
+		{ className: "border" },
+		NAME
+	),
+	React.createElement("img", { className: "border", src: "https://image-uploading-service.s3.us-west-1.amazonaws.com/1579935258989_linkedin-profile-compressed.jpg", width: "300px" }),
+	React.createElement(
+		"p",
+		null,
+		"A Fullstack Web and Android Developer from San Diego. International Graduate Student at San Diego State University pursuing Masters in Computer Science with an expected graduation date of May 2021. Actively seeking internships in Software Development for Summer 2020."
+	),
+	React.createElement(
+		"a",
+		{ href: "https://github.com/pavan-sdsu", target: "_blank" },
+		React.createElement(
+			"button",
+			{ className: "border" },
 			React.createElement(
-				"h1",
-				{ className: "border" },
-				NAME
-			),
-			React.createElement("img", { className: "border", src: "https://image-uploading-service.s3.us-west-1.amazonaws.com/1579935258989_linkedin-profile-compressed.jpg", width: "300px" }),
-			React.createElement(
-				"p",
+				"h3",
 				null,
-				"A Fullstack Web and Android Developer from San Diego. International Graduate Student at San Diego State University pursuing Masters in Computer Science with an expected graduation date of May 2021. Actively seeking internships in Software Development for Summer 2020."
-			),
-			React.createElement(
-				"a",
-				{ href: "https://github.com/pavan-sdsu", target: "_blank" },
-				React.createElement(
-					"button",
-					{ className: "border" },
-					React.createElement(
-						"h3",
-						null,
-						"VIEW MY GITHUB REPO"
-					)
-				)
+				"VIEW MY GITHUB REPO"
 			)
-		);
-	}
-}
+		)
+	)
+);
 
-ReactDOM.render(React.createElement(App), document.getElementById("root"));
+ReactDOM.render(element, document.getElementById('root'));
--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,14 @@
 const NAME = "Pavankumar Mahadik";
 
-class App extends React.Component {
-	render() {
-		return (
-			<div className="container border">
-				<h1 className="border">{NAME}</h1>
-				<img className="border" src="https://image-uploading-service.s3.us-west-1.amazonaws.com/1579935258989_linkedin-profile-compressed.jpg" width="300px" />
-				<p>A Fullstack Web and Android Developer from San Diego. International Graduate Student at San Diego State University pursuing Masters in Computer Science with an expected graduation date of May 2021. Actively seeking internships in Software Development for Summer 2020.</p>
-				<a href="https://github.com/pavan-sdsu" target="_blank">
-					<button className="border"><h3>VIEW MY GITHUB REPO</h3></button>
-				</a>
-			</div>
-		)
-	}
-}
+const element = (
+	<div className="container border">
+		<h1 className="border">{NAME}</h1>
+		<img className="border" src="https://image-uploading-service.s3.us-west-1.amazonaws.com/1579935258989_linkedin-profile-compressed.jpg" width="300px" />
+		<p>A Fullstack Web and Android Developer from San Diego. International Graduate Student at San Diego State University pursuing Masters in Computer Science with an expected graduation date of May 2021. Actively seeking internships in Software Development for Summer 2020.</p>
+		<a href="https://github.com/pavan-sdsu" target="_blank">
+			<button className="border"><h3>VIEW MY GITHUB REPO</h3></button>
+		</a>
+	</div>
+);
 
-ReactDOM.render(React.createElement(App), document.getElementById("root"));
+ReactDOM.render(element, document.getElementById('root'));
